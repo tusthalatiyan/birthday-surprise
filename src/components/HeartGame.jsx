480a23854e14c33f96ee45e54f7ded775c26d5f7
Tushar Latiyan <tusthalatiyan100@gmail.com>
2026-05-14 16:10:08 +0530

    Fix secret hunt popup behavior on mobile

--- src/components/HeartGame.jsx
+++ src/components/HeartGame.jsx
@@ -7,7 +7,7 @@ import { useWindowSize } from '../hooks/useWindowSize'
 
 const GAME_DURATION = 20
 const MOBILE_BREAKPOINT = 768
-const MOBILE_TARGET_SCORE = 20
+const MOBILE_TARGET_SCORE = 50
 const DESKTOP_TARGET_SCORE = 10
 
 export default function HeartGame({ onWin }) {
